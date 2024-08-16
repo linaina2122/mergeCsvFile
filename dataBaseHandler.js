a347c37466e8a1273902ba339f007125cb642999
unknown <hajarcharef2@gmail.com>
2024-08-16 15:08:55 +0200

    last version

---
+++ dataBaseHandler.js
@@ -0,0 +1,56 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('statistics',
+    'hcharef',
+    'Linaina@1998',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+    }
+);
+
+class PlanStatistics extends Model{}
+PlanStatistics.init(
+    {
+        VarName : {
+            type:DataTypes.TEXT,
+            allowNull:true
+        },
+        Time_string : {
+            type:DataTypes.TEXT,
+            allowNull:true
+        },
+        VarValue : {
+            type:DataTypes.TEXT,
+            allowNull:true
+        },
+        Validity : {
+            type:DataTypes.TEXT,
+            allowNull:true
+        },
+        Time_ms : {
+            type:DataTypes.TEXT,
+            allowNull:true
+        }
+    }, {
+    sequelize, 
+    modelName: 'PlanStatistics',
+    timestamps:false,
+});
+async function connectToDb( ) {
+    try {
+        await sequelize.authenticate();
+        console.log("database connected syccessfuly")
+    }
+    catch (error) {
+        console.log("couldn't connect")
+    }
+    try{
+        await sequelize.sync({alter:true})
+        console.log("The table for the PlanStatistics model was just (re)created")
+    }
+    catch(error){
+        console.log("couldn't create")
+    }
+}
+
+module.exports = {connectToDb, PlanStatistics};
---
+++ script.js
@@ -0,0 +1,99 @@
+
+const fs = require("fs");
+const { connectToDb, PlanStatistics
+ } = require("./dataBaseHandler");
+var buffer = "";
+
+function ReadFromFile(file) {
+  return new Promise((resolve, reject) => {
+    const src = fs.createReadStream(file, { encoding: "utf-8" })
+    src.on("data", (chunck) => {
+      if (!buffer) {
+        const index = chunck.indexOf("\n") + 1;
+        buffer += chunck.slice(index);
+      }
+      else
+        buffer += chunck;
+    })
+    src.on("end", () => {
+      console.log("---------no more data to read---------")
+      resolve(buffer);
+    })
+    src.on("close", () => {
+      console.log("*********flux is closed")
+    })
+    src.on("error", (error) => {
+      console.log("error while reading file", error);
+      reject();
+    })
+  })
+}
+
+async function AllDb(args) {
+  var index = false;
+  for (const file of args) {
+    if (file.includes(".csv")) {
+      index = true;
+      try {
+        await ReadFromFile(file);
+      }
+      catch (error) {
+        console.error("error", error)
+      }
+    }
+    else
+      continue;
+  }
+  if(index === false){
+    console.log("csv files not found");
+    process.exit()
+  }
+}
+
+async function FillDb() {
+  let array2d = [];
+  var i = 0;
+  var data = [];
+  let tmp;
+  array2d = buffer.split('\n');
+
+  while (i < array2d.length) {
+    array2d[i] = array2d[i].split(';')
+    
+    var j = 0;
+    while (j < array2d[i].length) {
+      tmp = {
+        VarName: array2d[i][j++],
+        Time_string: array2d[i][j++],
+        VarValue: array2d[i][j++],
+        Validity: array2d[i][j++],
+        Time_ms: array2d[i][j++]
+      };
+    }
+    data.push(tmp);
+
+    i++;
+  }
+    try {
+      await PlanStatistics.bulkCreate(data);
+      console.log("Data saved successfully");
+    } catch (error) {
+        console.error("Error while saving data:", error);
+      }
+  }
+
+
+
+
+async function main() {
+  
+  try {
+    await connectToDb(); 
+    await AllDb(process.argv);
+    await FillDb();
+  } catch (error) {
+    console.error("error occured:", error);
+  }
+}
+
+main();
